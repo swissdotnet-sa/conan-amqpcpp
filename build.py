38e79aa2ce129bac21b1a9b468e93c4f297ac710
SPerepelitsyn <stas.perepel@gmail.com>
2019-09-21 12:40:40 +0300

    Remove MT compile type for Win builds

--- build.py
+++ build.py
@@ -1,6 +1,13 @@
-from conan.packager import ConanMultiPackager
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import platform
+from bincrafters import build_template_default
+
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
-    builder.add_common_builds(shared_option_name="amqpcpp:shared", pure_c=False)
+    shared_option_name = False if platform.system() == "Windows"  or platform.system() == "Darwin" else "amqpcpp:shared"
+    builder = build_template_default.get_builder(pure_c=False, shared_option_name=shared_option_name)
+    if platform.system() == "Windows":
+        builder.remove_build_if(lambda build: "MT" in build.settings["compiler.runtime"])
     builder.run()
